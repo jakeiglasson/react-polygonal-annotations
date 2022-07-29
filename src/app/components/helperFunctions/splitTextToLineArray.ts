5e5853cf56697e4a0addc0dfdcee8c8f528a0be9
Jake GLasson <60338181+jakeiglasson@users.noreply.github.com>
2022-07-29 11:11:10 +0800

    simplify splitTextToLineArray function by removing unnecessary while loop
    add comments to function to explain how it works

--- src/app/components/helperFunctions/splitTextToLineArray.ts
+++ src/app/components/helperFunctions/splitTextToLineArray.ts
@@ -3,64 +3,45 @@ export const splitTextToLineArray = (text: string): { textSplitArray: string[]; 
 	const maxCharPerLine = 18;
 	let maxTextLineWidth = { width: 0, index: 0 };
 
-	if (text.length === 18) {
+	if (text.length === maxCharPerLine) {
 		return { textSplitArray: [text.trim()], maxTextLineWidthIndex: 0 };
 	} else if (text.length > maxCharPerLine) {
-		if (text.length > maxCharPerLine) {
-			const modifiedText = text
-				.split(" ")
-				.map((s) => {
-					if (s.length > 18) {
-						let modifiedStringArray: string[] = [];
-						const remainder = s.length % maxCharPerLine;
-						const multiple = (s.length - remainder) / maxCharPerLine;
-						if (multiple === 1) {
-							modifiedStringArray.push(s.slice(0, 18));
-							modifiedStringArray.push(s.slice(18, s.length));
-						} else {
-							for (let i = 0; i <= multiple; i++) {
-								const slice = s.slice(0 + 18 * i, 18 + 18 * i);
-								if (slice) {
-									modifiedStringArray.push(slice);
-								}
+		// modifiedText looks for strings within the given string that are longer than the given maxCharPerLine
+		// when such a string is found, it divides it into chunks, each chunk has a maximum possible length == maxCharPerLine
+		// example: input = '123456789a12345678zzz 123456789a12345678" output: '123456789a12345678 zzz 123456789a12345678'
+		// this string is then split on " " into an array so each value can be drawn to new line with canvas
+		const modifiedText = text
+			.split(" ")
+			.map((s, i) => {
+				if (s.length > 18) {
+					let modifiedStringArray: string[] = [];
+					const remainder = s.length % maxCharPerLine;
+					const multiple = (s.length - remainder) / maxCharPerLine;
+					if (multiple === 1) {
+						modifiedStringArray.push(s.slice(0, 18));
+						modifiedStringArray.push(s.slice(18, s.length));
+					} else {
+						for (let i = 0; i <= multiple; i++) {
+							const slice = s.slice(0 + 18 * i, 18 + 18 * i);
+							if (slice) {
+								modifiedStringArray.push(slice);
 							}
 						}
-						return modifiedStringArray.join(" ");
-					} else {
-						return s;
 					}
-				})
-				.join(" ");
-			text = modifiedText;
-		}
-		while (text.length > 18) {
-			let spaceIndex;
-			for (let i = 0; i < maxCharPerLine + 1; i++) {
-				if (text[i] === " " || text[i] === undefined) {
-					spaceIndex = i;
+					return modifiedStringArray.join(" ");
+				} else {
+					return s;
 				}
-				if (i > 0 && (i + 1) % (maxCharPerLine + 1) === 0) {
-					if (spaceIndex || spaceIndex === 0) {
-						textArray.push(text.slice(0, spaceIndex + 1));
-					} else {
-						textArray.push(text.slice(0));
-					}
-					if (textArray[textArray.length - 1].length > maxTextLineWidth.width) {
-						maxTextLineWidth.width = textArray[textArray.length - 1].length;
-						maxTextLineWidth.index = textArray.length - 1;
-					}
-					const startIndex = textArray[textArray.length - 1].length;
-					text = text.slice(startIndex);
-				}
+			})
+			.join(" ")
+			.replace(/\s\s+/g, " ");
+		textArray = modifiedText.split(" ");
+		textArray.forEach((s, i) => {
+			if (s.length > maxTextLineWidth.width) {
+				maxTextLineWidth.width = s.length;
+				maxTextLineWidth.index = i;
 			}
-		}
-		if (text.length > maxTextLineWidth.width) {
-			maxTextLineWidth.width = text.length;
-			maxTextLineWidth.index = textArray.length;
-		}
-		textArray.push(text);
-		textArray.forEach((v, i) => (textArray[i] = v.trim()));
-		textArray = textArray.filter((s) => s !== "");
+		});
 		return { textSplitArray: textArray, maxTextLineWidthIndex: maxTextLineWidth.index };
 	} else {
 		return { textSplitArray: [text], maxTextLineWidthIndex: 0 };
